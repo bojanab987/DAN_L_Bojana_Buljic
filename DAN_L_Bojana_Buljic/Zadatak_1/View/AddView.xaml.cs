7d97f07f5e69fa0abcbd11a857151a88eee3797f
bojanabb <bojana.buljic@gmail.com>
2020-08-05 14:10:44 +0200

    UserView and ViewModel implemented

---
+++ DAN_L_Bojana_Buljic/Zadatak_1/View/AddView.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace Zadatak_1.View
+{
+    /// <summary>
+    /// Interaction logic for AddView.xaml
+    /// </summary>
+    public partial class AddView : Window
+    {
+        public AddView()
+        {
+            InitializeComponent();
+            this.DataContext = new AddSongViewModel(this, user);
+        }
+    }
+}
---
+++ DAN_L_Bojana_Buljic/Zadatak_1/ViewModel/AddViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zadatak_1.Model;
+using Zadatak_1.Services;
+using Zadatak_1.View;
+
+namespace Zadatak_1.ViewModel
+{
+    class AddViewModel:ViewModelBase
+    {
+        AddView songView;
+        Service service = new Service();
+
+        private vwUser user;
+        public vwUser User
+        {
+            get
+            {
+                return user;
+            }
+            set
+            {
+                user = value;
+                OnPropertyChanged("User");
+            }
+        }
+
+        private vwSong song;
+
+        public vwSong Song
+        {
+            get
+            {
+                return song;
+            }
+            set
+            {
+                song = value;
+                OnPropertyChanged("Song");
+            }
+        }
+
+        public AddSongViewModel(AddSongView songView, vwUser user)
+        {
+            this.songView = songView;
+            User = user;
+            Song = new vwSong();
+        }
+    }
+}
