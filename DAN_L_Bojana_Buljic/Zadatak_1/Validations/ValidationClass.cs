8d4f1b9471f8a1a0fb381d2acc2a3c7859457677
bojanabb <bojana.buljic@gmail.com>
2020-08-05 12:38:58 +0200

    Validation class added

---
+++ DAN_L_Bojana_Buljic/Zadatak_1/Validations/ValidationClass.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Zadatak_1.Model;
+using Zadatak_1.Services;
+
+namespace Zadatak_1.Validations
+{
+    class ValidationClass
+    {
+        /// <summary>
+        /// This method checks if forwarded username is unique.
+        /// </summary>
+        /// <param name="username"></param>
+        /// <returns></returns>
+        public bool IsUniqueUsername(string username)
+        {
+            Service service = new Service();
+            List<tblUser> userList = service.GetAllUsers();
+            var list = userList.Where(x => x.Username == username).ToList();
+            //check if exists user with forwarded username
+            if (list.Count() > 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        /// <summary>
+        /// Checks if the password is correct
+        /// </summary>
+        /// <param name="password">the password we are checking</param>       
+        public string PasswordChecker(string password)
+        {
+            int upperCaseChars = 0;
+
+            //check if password lenght is minimum 6 characters
+            if (password == null || !(password.Length < 6))
+            {
+                return null;
+            }
+
+            //check if password contains 2 uppercase characters
+            for (int i = 0; i < password.Length; i++)
+            {
+                if (char.IsUpper(password[i]))
+                {
+                    upperCaseChars++;
+                    if (upperCaseChars == 2)
+                    {
+                        break;
+                    }
+                }
+            }
+
+            if (upperCaseChars < 2)
+            {
+                return null;
+            }
+            return password;
+        }
+    }
+}
